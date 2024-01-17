c659bdcda759a9603a45946f764f7f16d6996de0
strohne <krimskram@freenet.de>
2024-01-17 04:09:11 +0100

    Add text data, add documentation, disable two metrics

--- R/text_topics.R
+++ R/text_topics.R
@@ -7,7 +7,8 @@ lda_find_k <- function(dfm, sample.size=2000, plot=T) {
     quanteda::dfm_sample(min(quanteda::ndoc(.), sample.size)) %>%
     ldatuning::FindTopicsNumber(
       topics = c(c(2:9),seq(from = 10, to = 30, by = 5)),
-      metrics = c("Griffiths2004", "CaoJuan2009", "Arun2010", "Deveaud2014"),
+      #metrics = c("Griffiths2004", "CaoJuan2009", "Arun2010", "Deveaud2014"),
+      metrics = c("Griffiths2004", "CaoJuan2009"),
       control = list(seed = 77),
       mc.cores = 7L,
       verbose = TRUE
@@ -19,8 +20,8 @@ lda_find_k <- function(dfm, sample.size=2000, plot=T) {
 
   k <- mean(
     metrics$topics[which.max(metrics$Griffiths2004)],
-    metrics$topics[which.max(metrics$Deveaud2014)],
-    metrics$topics[which.min(metrics$Arun2010)],
+    #metrics$topics[which.max(metrics$Deveaud2014)],
+    #metrics$topics[which.min(metrics$Arun2010)],
     metrics$topics[which.min(metrics$CaoJuan2009)]
   )
 
@@ -30,13 +31,26 @@ lda_find_k <- function(dfm, sample.size=2000, plot=T) {
   )
 }
 
+#' Perform topic modeling on a text column and add the relevance scores of each topic to each row
+#'
+#' @param data A tibble
+#' @param col_text The column containing text
+#' @param prefix A prefix added to the new topic columns
+#' @param k The number of clusters / topics.
+#'          If NULL, the number will be determined by metrics from the ldatuning package()
+#' @param lambda The lambda value for relevance calculation of terms and example documents.
+#'               The higher the lambda, the more specific terms come out.
+#' @param seed A seed for the random number generator, makes results reproducible. You should change it!
+#' @param plot Whether to plot ldatuning results
+#' @return A data frame with a new column for each topic
+#' @export
 lda_add_topic <- function(data, col_text, prefix="tpc_", k=NULL, lambda=0.6, seed=1852, plot=F) {
 
   # Tokenize and convert to dfm
   ds_texts <- data %>%
-    mutate(doc = row_number()) %>%
-    select(doc, text={{col_text}}) %>%
-    drop_na()
+    dplyr::mutate(doc = dplyr::row_number())
+    dplyr::select(doc, text={{col_text}})
+    tidyr::drop_na()
 
   ds_tokens <- get_tokens(ds_texts)
   ds_dfm <- get_dfm(ds_tokens)
