c759a30dead49466fd8dbaf76f839b8448d06488
HenriekeKotthoff <100956909+HenriekeKotthoff@users.noreply.github.com>
2024-01-26 13:41:09 +0100

    Update rendering tests

--- tests/testthat/test-vignette.R
+++ tests/testthat/test-vignette.R
@@ -7,15 +7,13 @@ library(knitr)
 # Rendering tables vignette
 test_that("Tables in vignette render without error", {
   expect_no_error({
-    vignette_path <- system.file("vignettes", "tables.Rmd", package = "volker")
-    result <- rmarkdown::render(vignette_path)
+    result <- rmarkdown::render("_markdown/tables.Rmd")
   })
 })
 
 # Rendering plots vignette
 test_that("Plots in vignette render without error", {
   expect_no_error({
-    vignette_path <- system.file("vignettes", "plots.Rmd", package = "volker")
-    result <- rmarkdown::render(vignette_path)
+    result <- rmarkdown::render("_markdown/plots.Rmd")
   })
 })
