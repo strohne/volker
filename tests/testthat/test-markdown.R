fd14e2aad9ad928386a4f58099211b2bd002f5a8
strohne <krimskram@freenet.de>
2024-02-09 11:38:08 +0100

    Ignore image comparison on cran tests

--- tests/testthat/test-markdown.R
+++ tests/testthat/test-markdown.R
@@ -10,12 +10,28 @@ test_that("Tables in markdown documents render without error", {
   expect_snapshot_file(rmarkdown::render("_markdown/tables.Rmd"))
 })
 
-# Rendering plots
-test_that("Plots in markdown documents render without error", {
-  expect_snapshot_file(rmarkdown::render("_markdown/plots.Rmd"))
-})
 
-# Rendering reports
-test_that("Volker reports render without error", {
-  expect_snapshot_file(rmarkdown::render("_markdown/reports.Rmd"))
-})
+# Pixel accuracy is out of scope for our CRAN version by now.
+# Markdowns should run without errors, but we don't compare the images.
+if (identical(Sys.getenv("NOT_CRAN"), "true")) {
+  # Rendering plots
+  test_that("Plots in markdown documents render without error", {
+    expect_snapshot_file(rmarkdown::render("_markdown/plots.Rmd"))
+  })
+
+  # Rendering reports
+  test_that("Volker reports render without error", {
+    expect_snapshot_file(rmarkdown::render("_markdown/reports.Rmd"))
+  })
+} else {
+
+  # Rendering plots
+  test_that("Plots in markdown documents render without error", {
+    expect_no_error(rmarkdown::render("_markdown/plots.Rmd"))
+  })
+
+  # Rendering reports
+  test_that("Volker reports render without error", {
+    expect_no_error(rmarkdown::render("_markdown/reports.Rmd"))
+  })
+}
