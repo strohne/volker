c875ad622a76f92b2eb0caeab312a7dccf1e969a
HenriekeKotthoff <100956909+HenriekeKotthoff@users.noreply.github.com>
2024-03-12 18:10:46 +0100

    add macro statistics

--- R/stats.R
+++ R/stats.R
@@ -40,77 +40,20 @@ stats_metrics_one_grouped <- function(data, col, col_group, negative = FALSE, di
     data <- dplyr::mutate(data, dplyr::across({{ col }}, ~ dplyr::if_else(. < 0, NA, .)))
   }
 
-  data <- data |>
+  lm <- data |>
     dplyr::select(av = {{ col }}, uv ={{ col_group }})
 
-  fit <- stats::lm(av ~ uv, data= data)
+  fit <- stats::lm(av ~ uv, data= lm)
 
   result <- broom::tidy(fit)
 
+  # Add macro statistics
 
-  # result_grouped <- data %>%
-  #   dplyr::group_by({{ col_group }}) %>%
-  #   skim_metrics({{ col }}) %>%
-  #   dplyr::ungroup() %>%
-  #   dplyr::mutate(
-  #     {{ col_group }} := tidyr::replace_na(as.character({{ col_group }}), "Missing")
-  #   ) %>%
-  #   dplyr::select(-tidyselect::all_of(c("skim_variable","skim_type")))
-  #
-  # result_total <- data %>%
-  #   skim_metrics({{ col }}) %>%
-  #   dplyr::mutate({{ col_group }} := "Total")
-  #
-  # result <- dplyr::bind_rows(
-  #   result_grouped,
-  #   result_total
-  # ) %>%
-  #   dplyr::select(
-  #     {{ col_group }},
-  #     min = "numeric.min",
-  #     q1 = "numeric.q1",
-  #     median = "numeric.median",
-  #     q3 = "numeric.q3",
-  #     max = "numeric.max",
-  #     m = "numeric.mean",
-  #     sd = "numeric.sd",
-  #     "missing",
-  #     "n",
-  #     items = "numeric.items",
-  #     alpha = "numeric.alpha"
-  #   )
-  #
-  # # Remove items and alpha if not and index
-  # if (all(is.na(result$items)) || all(is.na(result$alpha))) {
-  #   result$items <- NULL
-  #   result$alpha <- NULL
-  # } else {
-  #   result <- result %>%
-  #     dplyr::mutate(dplyr::across(tidyselect::all_of("items"), ~ as.character(round(., 0)))) %>%
-  #     dplyr::mutate(dplyr::across(tidyselect::all_of("alpha"), ~ as.character(round(., 2))))
-  # }
-  #
-  # # Get item label from the attributes
-  # if (labels) {
-  #   codes <- data %>%
-  #     codebook({{ col_group }}) %>%
-  #     dplyr::distinct(dplyr::across(tidyselect::all_of(c("item_name", "item_label")))) %>%
-  #     stats::na.omit()
-  #
-  #   if (nrow(codes) > 0) {
-  #     label <- codes$item_label[1]
-  #     result <- result %>%
-  #       dplyr::rename({{ label }} := {{ col_group }})
-  #   }
-  #
-  #   scale <- attr(dplyr::pull(data, {{ col }}), "scale")
-  #   if (is.null(scale)) {
-  #     scale <- data %>%
-  #       codebook({{ col }}) %>%
-  #       dplyr::distinct(dplyr::across(tidyselect::all_of(c("value_name", "value_label"))))
-  #   }
-  #   attr(result, "scale")
-  # }
+  result_macro <- broom::glance(fit)
 
-  .to_vlkr_tab(result, digits= digits)
+  # Rename result labels
+
+  .to_vlkr_tab(result,digits= digits)
+
+  .to_vlkr_tab(result_macro, digits= digits)
 }
