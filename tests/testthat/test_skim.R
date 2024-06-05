2a3c3a6d59d233905cc9b15ee0bfc27c8c275d46
HenriekeKotthoff <100956909+HenriekeKotthoff@users.noreply.github.com>
2024-06-05 14:25:35 +0200

    add tests for checks and skim

---
+++ tests/testthat/test_skim.R
@@ -0,0 +1,14 @@
+#
+# Skim tests
+#
+
+library(testthat)
+library(volker)
+
+# Load and recode data
+data <- volker::chatgpt
+
+test_that("skim_metrics",{
+         skim_metrics(data)
+         expect_snapshot(data)
+})
