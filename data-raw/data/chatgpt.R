63c76a3f7d4ee6e94a3caddccee8d44afcda99fd
Jakob Jünger <jakob.juenger@uni-muenster.de>
2025-10-03 17:32:55 +0200

    Translate activities in example data, add manual codings

--- data-raw/data/chatgpt.R
+++ data-raw/data/chatgpt.R
@@ -5,36 +5,9 @@
 library(tidyverse)
 library(volker)
 
-# chatgpt <- readr::read_rds("data-raw/data/chatgpt.rds")
-#
-# # Rename vars
-# chatgpt <- chatgpt |>
-#   rename(
-#     use_private = cg_nutzen_privat,
-#     use_work = cg_nutzen_beruflich,
-#     adopter = in_adoption,
-#     sd_gender = sd_geschlecht,
-#     sd_age = sd_alter
-#   )
-#
-# #Add label to sd_age
-# chatgpt <- labs_apply(
-#   chatgpt,
-#   tibble::tribble(
-#     ~item_name, ~item_value,
-#     "sd_age", "Age",
-#     "adopter", "Innovator type"
-#   )
-# )
-
-# Translate dataset
-#write_csv2(chatgpt,"data-raw/chatgpt_de.csv", na="")
-
-# Translate codebook
-#codes_de <- codebook(chatgpt)
-#write_csv2(codes_de,"data-raw/codes_de.csv", na="")
-
 chatgpt_en <- read_csv2("data-raw/data/chatgpt_en.csv", na="")
+
+# Set labels and categories
 chatgpt_en$sd_gender <- factor(chatgpt_en$sd_gender, levels=c("female","male","diverse","[no answer]"))
 chatgpt_en$adopter <- factor(chatgpt_en$adopter, levels = c(
   "I try new offers immediately",
@@ -43,7 +16,6 @@ chatgpt_en$adopter <- factor(chatgpt_en$adopter, levels = c(
   "I only use new offers when I have no other choice",
   "[no answer]"
 ))
-
 
 
 codes_en <- read_csv2("data-raw/data/codes_en.csv", na="",col_types = "c")
@@ -63,11 +35,11 @@ chatgpt_en <- chatgpt_en |>
 
 nonascii <- chatgpt_en |>
   select(cg_activities) |>
-  mutate(chars = str_extract_all(cg_activities,"[^a-zA-Z0-9\",;\\.<> /\\\\\\?\\(\\)-]")) |>
+  mutate(chars = str_extract_all(cg_activities,"[^a-zA-Z0-9\'\",;\\.<> /\\\\\\?\\(\\)-]")) |>
   filter(sapply(chars,\(x) length(x) > 0))
 
 stopifnot(nrow(nonascii) == 0)
 
-# Use
+# Use (uncomment to update the data set)
 #chatgpt <- chatgpt_en
 #usethis::use_data(chatgpt, overwrite = TRUE)
