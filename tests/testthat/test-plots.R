eb20b56f73f07cb16b091745295655bab778d616
strohne <krimskram@freenet.de>
2025-10-07 20:15:34 +0200

    Add visual plot test with vdiffr

--- tests/testthat/test-plots.R
+++ tests/testthat/test-plots.R
@@ -2,8 +2,25 @@
 # Test plot features
 #
 
+library(vdiffr)
 library(testthat)
 library(volker)
+
+# By default, if vdiffr is not installed, all visual tests are skipped unless
+# VDIFFR_RUN_TESTS is explicitly set to "true", which should be the case only on
+# a GitHub Actions CI runner with stable version of R.
+
+if (requireNamespace("vdiffr", quietly = TRUE) && utils::packageVersion('testthat') >= '3.0.3') {
+  expect_doppelganger <- vdiffr::expect_doppelganger
+} else {
+  # If vdiffr is not available and visual tests are explicitly required, raise error.
+  if (identical(Sys.getenv("VDIFFR_RUN_TESTS"), "true")) {
+    abort("vdiffr is not installed")
+  }
+
+  # Otherwise, assign a dummy function
+  expect_doppelganger <- function(...) skip("vdiffr is not installed.")
+}
 
 # Load the sample data
 data <- volker::chatgpt
@@ -21,54 +38,50 @@ data <- volker::chatgpt
 #
 
 
-if (Sys.getenv("R_LOCALTESTS") == "1") {
+#if (Sys.getenv("R_LOCALTESTS") == "1") {
 
   test_that("boxplot", {
 
-    library(ggplot2)
-
     p <- plot_metrics(data, sd_age, box = T)
+    expect_doppelganger("Univariable box plot", p)
 
     # Save the plot to a temporary file
-    plot_file <- tempfile(fileext = ".png")
-    ggsave(plot_file, plot = p)
+    #plot_file <- tempfile(fileext = ".png")
+    #ggsave(plot_file, plot = p)
 
     # Capture the plot as a snapshot
-    expect_snapshot_file(plot_file, "plot_metrics_box.png")
+    #expect_snapshot_file(plot_file, "plot_metrics_box.png")
 
   })
 
-  test_that("applying boxplot to metric plot", {
-
-    library(ggplot2)
+  test_that("Univariable denisity plot with CI", {
 
     p <- plot_metrics(data, sd_age, ci = TRUE)
+    expect_doppelganger("Univariable density plot with CI", p)
 
-    # Save the plot to a temporary file
-    plot_file <- tempfile(fileext = ".png")
-    ggsave(plot_file, plot = p)
-
-    # Capture the plot as a snapshot
-    expect_snapshot_file(plot_file, "plot_metrics_ci.png")
+    # # Save the plot to a temporary file
+    # plot_file <- tempfile(fileext = ".png")
+    # ggsave(plot_file, plot = p)
+    #
+    # # Capture the plot as a snapshot
+    # expect_snapshot_file(plot_file, "plot_metrics_ci.png")
 
   })
 
-  test_that("applying boxplot to categorical plot", {
-
-    library(ggplot2)
+  test_that("Univariable bar plot with CI", {
 
     p <- plot_counts(data, sd_gender, ci = TRUE)
-
-    # Save the plot to a temporary file
-    plot_file <- tempfile(fileext = ".png")
-    ggsave(plot_file, plot = p)
-
-    # Capture the plot as a snapshot
-    expect_snapshot_file(plot_file, "plot_counts_ci.png")
+    expect_doppelganger("Univariable bar plot with CI", p)
+    # # Save the plot to a temporary file
+    # plot_file <- tempfile(fileext = ".png")
+    # ggsave(plot_file, plot = p)
+    #
+    # # Capture the plot as a snapshot
+    # expect_snapshot_file(plot_file, "plot_counts_ci.png")
 
   })
 
-}
+#}
 
 test_that("Empty plots are empty", {
    test_data <- tibble(var1=c(NA))
